f0a9acf9394d6a02e2fc16c545b0cc95a08605a7
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-05-03 09:20:40 -0700

    update

--- index.php
+++ index.php
@@ -5,7 +5,28 @@ $name = "JOHN is going to school h";
 echo "$name";
 
 
+//PHP data types
+/*
+String - text
+integer - number
+float-doule
+array
+null
+Boolean
+
+*/ 
+
+
+//String
+$words = "Hello Word";
+$Page =  "This my page";
+echo gettype($Page);  //  this help you to know the kind of datatype you using
+
+//Integer
+$age = 40;
+
+
 ?>
 
-
-
+<h2>i love <?php  echo"$words"; ?> </h2>
+<h2>i love <?php  echo"$Page"; ?> </h2>
