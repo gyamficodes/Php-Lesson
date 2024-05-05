444ef24448008ea1dfffedfd01663d659936607d
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-05-05 02:20:54 -0700

    update

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php 
 
-
 $name = "JOHN is going to school h";
 echo "$name";
-
 
 //PHP data types
 /*
@@ -15,7 +13,6 @@ null
 Boolean
 
 */ 
-
 
 //String
 $words = "Hello Word";
@@ -35,7 +32,7 @@ echo gettype($isLogin);
 
 //array
 $student = array("john", "Yaw", "John", "Apraku");
-    echo  Var_dump($student);
+echo  Var_dump($student);
 
 for($i = 0; $i <10 ; $i++){
     echo $i;
@@ -47,8 +44,31 @@ $max = max(20, 15 , 1 ,  70);
 echo $min;
 echo $max;
 
+$sqt = sqrt(64);
+echo $sqt; 
+
+// $rand = rand();
+// echo $rand;
+//MD array
+
+$student = array("name" => array('John',"kofi"), "age" => 20, "gender" => "male");
+echo Var_dump($student);
+
+
+//constant varriable
+// define("name", "Gyamfi John");
+define("Mylist" ,array("codes","jonny" , "come"));
+
+echo var_dump(Mylist);
+// echo name;
+
+
+//if eslw
+$age = 40;
+
 
 ?>
 
 <h2>i love <?php  echo"$words"; ?> </h2>
 <h2>i love <?php  echo"$Page"; ?> </h2>
+
