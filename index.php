2a30e171395c7fd4e56826df5fe6fe9a88dd5355
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-05-12 15:01:47 -0700

    UPDATE

--- index.php
+++ index.php
@@ -398,6 +398,12 @@ $toyota = new toyota("Toyota");
 // echo $toyota->SMM();
 
 
+ //Interface
+
+ /**PHP - What are Interfaces?
+Interfaces allow you to specify what methods a class should implement.
+Interfaces make it easy to use a variety of different classes in the same way. When one or more classes use the same interface, it is referred to as "polymorphism".
+Interfaces are declared with the interface keyword: */
 
 
 
