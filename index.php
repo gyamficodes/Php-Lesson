779e49867aa733cf30fca589f3de854f753682ee
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-05-16 06:42:00 -0700

    I have started learning traits.

--- index.php
+++ index.php
@@ -526,6 +526,27 @@ foreach($zoos as $Zoo){
 
 
 
+/**
+ * PHP - What are Traits?
+PHP only supports single inheritance: a child class can inherit only from one single parent.
+So, what if a class needs to inherit multiple behaviors? OOP traits solve this problem.
+Traits are used to declare methods that can be used in multiple classes. Traits can have methods and abstract methods that can be used in multiple classes, and the methods can have any access modifier (public, private, or protected).
+Traits are declared with the trait keyword:
+ */
+
+trait hotels{
+  public function someNews(){
+    echo "this is some news about hotel";
+  }
+}
+
+class TapHotel{
+  use hotels;
+}
+
+$tabhotels = new TapHotel();
+$tabhotels->someNews();
+
 
 
 
