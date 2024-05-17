20f9ac8f6e3b551b2eac412151a9d78d8c9d40a5
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-05-17 16:24:01 -0700

    updated

--- index.php
+++ index.php
@@ -645,11 +645,55 @@ $pray->ShowPrayer();
  $p2->MyBoxes();
 
 
- 
+//  To call a static property from a child class, use the parent keyword inside the child class:
+
+class Voltic {
+  public static $V1 = "Voltic Boottle";
+}
+
+
+class Ven extends Voltic{
+    public function maieVer(){
+      return  parent::$V1;
+    }
+}
+$Verna1 = new Ven();
+echo $Verna1->maieVer();
 
 
 
 
+//Implement the Iterator interface and use it as an iterable:
+class MyIterator implements Iterator {
+  private $items = [];
+  private $pointer = 0;
+
+  public function __construct($items) {
+    // array_values() makes sure that the keys are numbers
+    $this->items = array_values($items);
+  }
+
+  public function current() {
+    return $this->items[$this->pointer];
+  }
+
+  public function key() {
+    return $this->pointer;
+  }
+
+  public function next() {
+    $this->pointer++;
+  }
+
+  public function rewind() {
+    $this->pointer = 0;
+  }
+
+  public function valid() {
+    // count() indicates how many items are in the list
+    return $this->pointer < count($this->items);
+  }
+}
 
 
 
